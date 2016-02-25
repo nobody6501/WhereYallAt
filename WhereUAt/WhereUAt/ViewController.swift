aef0902e6728bfd3d5dacd603b47ed5e205b326c
angelxut <angelx@utexas.edu>
2016-02-25 17:12:22 -0600

    init commit for maps

--- WhereUAt/WhereUAt/ViewController.swift
+++ WhereUAt/WhereUAt/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
-
+import GoogleMaps
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //        let mapView = self.view as! GMSMapView
+        var camera = GMSCameraPosition.cameraWithLatitude(-33.86, longitude: 151.20, zoom: 10)
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        var marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
